7d0c01cc3007a9b993b63474539070b0df1df72b
Catherine Tu <tucatherine.95@gmail.com>
2015-10-16 15:33:19 -0500

    Added .csv on Public Schools. Performed ETL functions on Public Schools data.

--- 01 Data/R_ETL_PublicSchools.R
+++ 01 Data/R_ETL_PublicSchools.R
@@ -1,37 +1,35 @@
-#Using one ETL File per CSV, change your measures AND change dfE to be dfC for crimes dfS for Socioeconomic etc Just follow example in other ETL files...
 require(tidyr)
 require(dplyr)
 require(ggplot2)
 
-#setwd("C:\\Users\\hyunji\\Documents\\DataVisualization\\DV_RProject3\\01 Data\\CSVs")
+setwd("C:/Users/Chia-LinCatherine/Documents/DataVisualization/DrCannata/DV_RProject3/01 Data/CSVs")
 
-file_path <- "Chicago_PublicHealthIndicators.csv"
+file_path <- "Chicago_PublicSchoolsProgressReportCards.csv"
 
-dfE <- read.csv(file_path, stringsAsFactors = FALSE)
+df <- read.csv(file_path, stringsAsFactors = FALSE)
 
 # Replace "." (i.e., period) with "_" in the column names.
-names(dfE) <- gsub("\\.+", "_", names(dfE))
+names(df) <- gsub("\\.+", "_", names(df))
 
-#str(dfE) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+# str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 
-measures <- c("Community_Area", "Birth_Rate", "General_Fertility_Rate", "Low_Birth_Weight", "Prenatal_Care_First_Trimester", "Preterm_Births", "Teen_Birth_Rate", "Assault_Homocide", "Breast_Cancer_Fem", "Cancer_All_Sites", "Colorectal_Cancer", "Diabetes_related", "Firearm_related", "Inf_Mort_Rate", "Lung_Cancer","Prostate_Cancer_M", "Stroke_Cerebrovascular_Disease", "Child_Blood_Lv_Screening", "Child_Lead_Poisoning", "Ghonorrhea_in_Females", "Ghonorrhea_in_Males", "Tuberculosis", "Below_Poverty_Level", "Crowded_Housing", "Dependency", "No_HS_Diploma", "Per_Capita_Income", "Unemployment")
-#measures <- NA # Do this if there are no measures.
+measures <- c("Safety_Score", "Family_Score", "Environment_Score", "Instruction_Score", "Teachers_Score", "Parent_Engage_Score", "Parent_Env_Score", "Avg_Student_Atd", "Misconduct_Rate" , "Avg_Teacher_Atd", "ISAT_Exceed_Math", "ISAT_Exceed_Reading", "College_Enrollment" )
 
 # Get rid of special characters in each column.
 # Google ASCII Table to understand the following:
-for(n in names(dfE)) {
-  dfE[n] <- data.frame(lapply(dfE[n], gsub, pattern="[^ -~]",replacement= ""))
+for(n in names(df)) {
+  df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
 }
 
-dimensions <- setdiff(names(dfE), measures)
+dimensions <- setdiff(names(df), measures)
 if( length(measures) > 1 || ! is.na(dimensions)) {
   for(d in dimensions) {
     # Get rid of " and ' in dimensions.
-    dfE[d] <- data.frame(lapply(dfE[d], gsub, pattern="[\"']",replacement= ""))
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
     # Change & to and in dimensions.
-    dfE[d] <- data.frame(lapply(dfE[d], gsub, pattern="&",replacement= " and "))
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
     # Change : to ; in dimensions.
-    dfE[d] <- data.frame(lapply(dfE[d], gsub, pattern=":",replacement= ";"))
+    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
   }
 }
 
@@ -41,12 +39,12 @@ if( length(measures) > 1 || ! is.na(dimensions)) {
 # Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
 if( length(measures) > 1 || ! is.na(measures)) {
   for(m in measures) {
-    dfE[m] <- data.frame(lapply(dfE[m, ], gsub, pattern="[^--.0-9]",replacement= ""))
-
+    df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
+    
   }
 }
 
-write.csv(dfE, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
 
 tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
 sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
--- 01 Data/R_Load_PublicSchools.R
+++ 01 Data/R_Load_PublicSchools.R
@@ -2,7 +2,7 @@ require("jsonlite")
 require("RCurl")
 
 #Gather data from database fill into local data frame and retrieve headers and summary 
-dfEd <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from Chicago_Education"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_rm46926', PASS='orcl_rm46926', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+dfPS <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from Chicago_PublicSchools"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_rm46926', PASS='orcl_rm46926', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-head(dfEd)
-summary(dfEd)
+head(dfPS)
+summary(dfPS)
