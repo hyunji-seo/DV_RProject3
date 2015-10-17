217a4455cbe39bf833e31e6d7eb273be57b688ad
hyunji-seo <hjseo767@gmail.com>
2015-10-16 22:14:59 -0500

    Finished Socioeconomic.R and updated SE portion of Rmd

--- 01 Data/R_Load_Socioeconomic.R
+++ 01 Data/R_Load_Socioeconomic.R
@@ -5,4 +5,4 @@ require("RCurl")
 dfSocio <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from Chicago_Socioeconomic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_rm46926', PASS='orcl_rm46926', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 head(dfSocio)
-summary(dfSocio)
+#summary(dfSocio)
--- 02 Data Wrangling/SocioEconomic_Crimes.R
+++ 02 Data Wrangling/SocioEconomic_Crimes.R
@@ -1,1 +1,27 @@
-Fill with things
+require(tidyr)
+require(dplyr)
+require(extrafont)
+require(ggplot2)
+
+ndfCr <- dfCr %>% group_by(PRIMARY_TYPE, COMMUNITY_AREA) %>% summarise(COUNT = n()) %>% arrange(COMMUNITY_AREA)
+#View(ndfCr)
+
+pdC <- ndfCr
+pdS <- dfSocio
+ndf<- dplyr::right_join(pdC, pdS, by="COMMUNITY_AREA")
+SEC <- ndf%>%  filter(PRIMARY_TYPE=='BATTERY' | PRIMARY_TYPE=='BURGLARY' | PRIMARY_TYPE=='CRIMINAL DAMAGE' | PRIMARY_TYPE=='DECEPTIVE PRACTICE' | PRIMARY_TYPE=='NARCOTICS' | PRIMARY_TYPE =='OTHER OFFENSE' | PRIMARY_TYPE=='ROBBERY' | PRIMARY_TYPE=='THEFT') 
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  labs(title='Chicago Data') +
+  labs(x=paste("Type of Crime"), y=("Community Area Number"), color=("Hardship Index")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  layer(data=SEC, 
+        mapping=aes(x=as.character(PRIMARY_TYPE), y=as.numeric(COMMUNITY_AREA), color=HARDSHIP_INDEX),
+        stat="identity",
+        stat_params=list(),
+        geom="point",
+        geom_params=list(),
+        position=position_jitter(width=0.2, height=0)
+  )
