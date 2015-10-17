09180a5ca4746ce069a0b0f5dfac5095574a3435
Catherine Tu <tucatherine.95@gmail.com>
2015-10-16 22:18:37 -0500

    Completed visualization of Public Schools rating vs Crime

--- 02 Data Wrangling/Education_Crimes.R
+++ 02 Data Wrangling/Education_Crimes.R
@@ -1,1 +1,30 @@
-fill with things
+require(tidyr)
+require(dplyr)
+require(extrafont)
+require(ggplot2)
+
+# Summarize crimes by community area
+ndfCr <- dfCr %>% group_by(COMMUNITY_AREA,PRIMARY_TYPE) %>% summarise(CRIME_COUNT = n())
+
+# Re-order safety levels
+dfPS$SAFETY_LEVEL <- factor(dfPS$SAFETY, c("Very Weak","Weak", "Average", "Strong", "Very Strong", "NDA"))
+
+# Join
+dfCrPS <- inner_join(dfPS, ndfCr, by = c("COMMUNITY_AREA"="COMMUNITY_AREA")) %>% select(PRIMARY_TYPE, SAFETY_LEVEL, MISCONDUCT_RATE, CRIME_COUNT) %>% filter(PRIMARY_TYPE == "NARCOTICS")
+
+#Create the Point Plot
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  # facet_grid(PR.) +
+  labs(title='Public School Safety Rating and Misconduct Rate with Narcotic Abuse in the Community Area') +
+  labs(x="Safety Rating", y=paste("Misconduct Rate (Per 1000 Students)")) +
+  layer(data=dfCrPS, 
+        mapping=aes(x=as.character(SAFETY_LEVEL), y=as.numeric(MISCONDUCT_RATE), color=SAFETY_LEVEL),
+        stat="identity",
+        stat_params=list(),
+        geom="point",
+        geom_params=list(alpha=.55), 
+        position=position_jitter(width=0.2)
+  )
